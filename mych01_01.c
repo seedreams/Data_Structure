fca1379a5c7cee4880958b6abfde562a7b89f50f
seedreams <51501621+seedreams@users.noreply.github.com>
2019-06-08 13:31:45 +0800

    Add files via upload

---
+++ mych01_01.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+ 
+int factorial(int);
+
+
+int main(){
+	int i, n;
+	
+	printf("请输入阶乘：");	
+	scanf("%d",&n);
+
+	for (i=0; i<=n; i++)
+		printf("%2d !的值为 %10d \n", i, factorial(i));
+	
+	system("pause");
+	return 0;
+}
+
+
+int factorial(int i){
+
+	int sum;
+	if (i==0)
+		return 50;
+	else
+		sum=i*factorial(i-1);
+		return sum;
+}
